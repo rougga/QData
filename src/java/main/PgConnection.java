0f84871462a03a0d5d5d0b81545bf4402d724918
ROUGGA <rouggama@gmail.com>
2023-02-27 16:57:44 +0100

    removing test param

--- src/java/main/PgConnection.java
+++ src/java/main/PgConnection.java
@@ -9,12 +9,12 @@ import java.sql.SQLException;
 public class PgConnection {
 
    private final String DRIVER = "org.postgresql.Driver";
-   // private final String CONNECTIONSTRING = "jdbc:postgresql://localhost:5432/postgres";
-  // private final String USER = "honyi";
-   // private final String PASSWORD = "honyi123";
-    private final String CONNECTIONSTRING = "jdbc:postgresql://localhost:5555/postgres";
-    private final String USER = "postgres";
-    private final String PASSWORD = "123456";
+   private final String CONNECTIONSTRING = "jdbc:postgresql://localhost:5432/postgres";
+   private final String USER = "honyi";
+    private final String PASSWORD = "honyi123";
+   //  private final String CONNECTIONSTRING = "jdbc:postgresql://localhost:5555/postgres";
+  //  private final String USER = "postgres";
+   // private final String PASSWORD = "123456";
     private Connection con=null;
     private Statement st=null;
 
