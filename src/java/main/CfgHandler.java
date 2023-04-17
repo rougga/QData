3019c3c348cfb0ed1c9e27000ec92a0c64952e30
ROUGGA <rouggama@gmail.com>
2023-04-17 14:26:12 +0000

    17-04-2023

--- src/java/main/CfgHandler.java
+++ src/java/main/CfgHandler.java
@@ -22,7 +22,7 @@ public class CfgHandler {
     
     //MetaData
     public  static final String APP = "QData";
-    public static final String VERSION = "0.3.1";
+    public static final String VERSION = "0.4";
     public static final String COMPANY = "ROUGGA";
     public static final String CLIENT = "";
     public static final int APP_PORT=8989;
